8bd060bfc72fa1937c20cc0c60eb8fc241f07230
Tim Elgersma <timothy.elgersma@gmail.com>
2019-01-26 15:16:35 -0500

    Included SQLAlchemy

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
