4a12a2f63d660b7e4e8abd1eb4d93209750751fa
Tim Elgersma <timothy.elgersma@gmail.com>
2019-01-26 19:22:24 -0500

    Fixed ipython model importing

--- FlaskApp/models/__init__.py
+++ FlaskApp/models/__init__.py
@@ -1,3 +1,6 @@
-from FlaskApp.app import db
+if __package__ is None: # ipython
+    from app import db
+else:                   # python app.py
+    from ..app import db
 
 all = ["roommates", "purchases", "ledgers"]
