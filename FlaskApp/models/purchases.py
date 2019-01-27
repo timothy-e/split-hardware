13e5ec36ecf38b44245a8ba8cacdcdf52e81b16a
Tristan Le <tristan.tuanle@gmai.com>
2019-01-26 20:40:18 -0500

    Fixed ForeignKey(...) -> db.ForeignKey(...)

--- FlaskApp/models/purchases.py
+++ FlaskApp/models/purchases.py
@@ -4,4 +4,4 @@ class Purchase(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     item_name = db.Column(db.String(120))
     cost = db.Column(db.Float)
-    buyer_id = db.Column(ForeignKey('Roommate.id'))
+    buyer_id = db.Column(db.ForeignKey('Roommate.id'))
