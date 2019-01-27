a2f0c7bed0e1ec853607b62b4b82cc751f3fe3eb
Tim Elgersma <timothy.elgersma@gmail.com>
2019-01-27 06:52:41 -0500

    Various QOL/bug fixes, idk its 7am

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from dataRoommate import *
+from dataRoommate import Roommate, Purchase, House
 
 
 app = Flask(__name__)
-
-#########################################
-#                                       #
-#  define models here (python classes)  #
-#                                       #
-#########################################
 
 Evan = Roommate("Evan")
 Tristan = Roommate("Tristan")
@@ -23,41 +17,40 @@ home.add_member(Tim)
 home.add_member(Tristan)
 home.add_member(Emmy)
 
+
 @app.route("/")
 def main():
-    return render_template('index.html', id=None)
+    return render_template("index.html", id=None)
 
-@app.route("/add_purchase/", methods=["POST"]) # pass in params by cURL: name, cost, item
+
+@app.route("/add_purchase/", methods=["POST"])  # pass in params by cURL: name, cost, item
 def add_purchase():
-    name = request.args.get('name')
-    cost = request.args.get('cost')
-    item = request.args.get('item')
+    name = request.args.get("name")
+    cost = request.args.get("cost")
+    item = request.args.get("item")
     # add a purchase to the Roommate with name=name
-    
-    for member in house.members:
-        if member.roommate == name:
+
+    for member in home.members:
+        if member.name == name:
             currentRoommate = member
-        
-    item = Purchase("item", cost)
+
+    item = Purchase(item, float(cost))
     currentRoommate.add_item(item)
-    pass
+    return item.as_json()
 
 
 @app.route("/settle_debts/", methods=["POST"])
 def settle_debts():
     # reset all balances to 0
-    for member in house.members:
+    for member in home.members:
         member.set_to_zero()
-    pass
+    return home.as_json()
+
 
 @app.route("/get_balance/<identity>", methods=["GET"])
 def get_balance(identity):
+    return render_template("debt.html", home=home, identity=identity)
 
-    return render_template('debt.html', home = home, identity = identity)
-    # create a page with all debt info for Roommate with name=name
-    # query for Roommate
-    # pass into a render template
-    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
--- FlaskApp/dataRoommate.py
+++ FlaskApp/dataRoommate.py
@@ -1,35 +1,59 @@
-class Roommate:
+import json
 
+
+class Serializable:
+    def as_json(self):
+        return json.dumps(dir(self))
+
+
+class Roommate(Serializable):
     def __init__(self, name):
-        self.roommate = name
+        self.name = name
         self.purchases = []
         self.balance = 0
+        self.cleared_index = 0
 
     def add_item(self, item):
         self.purchases.append(item)
         self.accumulate()
         return self.balance
 
+    @property
+    def cleared_purchases(self):
+        return self.purchases[: self.cleared_index]
+
+    @property
+    def uncleared_purchases(self):
+        return self.purchases[self.cleared_index :]
+
     def accumulate(self):
-        self.balance = 0
-        for purchase in self.purchases:
-            self.balance += purchase.amount
+        self.balance = sum(purchase.amount for purchase in self.purchases[self.cleared_index :])
         return self.balance
-    
+
     def set_to_zero(self):
         self.balance = 0
-    
+        self.cleared_index = len(self.purchases)
+        print(self.purchases)
+
     def get_owing(self, average):
         owe = average - self.balance
         if average - self.balance < 0:
-            print(f"Everyone owes {self.roommate} ${abs(owe)}")
+            print(f"Everyone owes {self.name} ${abs(owe)}")
 
         else:
-            print(f"{self.roommate} owes everyone ${owe}")
+            print(f"{self.name} owes everyone ${owe}")
         return owe
 
-class House:
+    def pretty_owing(self, average):
+        owe = self.get_owing(average)
+        pretty_owe = f"${abs(owe):0.02f}"
+        if owe <= 0:
+            return f"The house hold owes {self.name} {pretty_owe}."
+        else:
+            return f"{self.name} owes the house hold {pretty_owe}."
 
+
+class House(Serializable):
     def __init__(self, name):
         self.unit = name
         self.members = []
@@ -38,53 +62,23 @@ class House:
         self.members.append(item)
 
     def get_total_amount(self):
-        total = 0
-        for member in self.members:
-            total += member.accumulate()
-
-        return total
+        return sum(member.accumulate() for member in self.members)
 
     def get_average(self):
-        return self.get_total_amount()/len(self.members)
+        return self.get_total_amount() / len(self.members)
 
-class Purchase:
+    def get_owed(self, name):
+        my_balance = next(m for m in self.members if m.name == name).balance
+        others = [m for m in self.members if m.name != name]
 
+        return {other.name: f"${(my_balance - other.balance) / len(self.members):0.02f}" for other in others}
+
+
+class Purchase(Serializable):
     def __init__(self, name, amount):
         self.item = name
         self.amount = amount
 
-def main():
-    bread = Purchase("Bread", 20)
-    toiletPaper = Purchase("Toilet Paper", 50)
-    rice = Purchase("Rice", 100)
-
-    Evan = Roommate("Evan bot")
-    Tristan = Roommate("Tristan bot")
-    Tim = Roommate("Tim bot")
-    Emmy = Roommate("Emmy bot")
-
-    print("Each member's spending:")
-    print(Evan.add_item(bread))
-    print(Evan.add_item(toiletPaper))
-    print(Tristan.add_item(rice))
-    print(Emmy.add_item(toiletPaper))
-    print(Tim.add_item(bread))
-
-    home = House("R204")
-    home.add_member(Evan)
-    home.add_member(Tim)
-    home.add_member(Tristan)
-    home.add_member(Emmy)
-
-    total = 0
-    for members in home.members:
-        total += members.accumulate()
-    print(home.get_average())
-
-    get_owing("Evan", Evan.balance, home.get_average())
-    get_owing("Emmy", Emmy.balance, home.get_average())
-    get_owing("Tim", Tim.balance, home.get_average())
-    get_owing("Tristan", Tristan.balance, home.get_average())
-
-if __name__ == "__main__":
-    main()
+    @property
+    def pretty_amount(self):
+        return f"${self.amount:0.2f}"
